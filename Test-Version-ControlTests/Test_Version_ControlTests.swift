f29c44c6099fc7906c39234259f46d1a71694bfe
Rauf Pratama <rauf@wetakeover.co>
2021-02-09 16:37:03 +0700

    Initial Commit

---
+++ Test-Version-ControlTests/Test_Version_ControlTests.swift
@@ -0,0 +1,33 @@
+//
+//  Test_Version_ControlTests.swift
+//  Test-Version-ControlTests
+//
+//  Created by Muhammad Rauf Pratama on 09/02/21.
+//
+
+import XCTest
+@testable import Test_Version_Control
+
+class Test_Version_ControlTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
